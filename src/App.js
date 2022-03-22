b539c091664fa78a8196832124fe7fb8e6d10ecd
ephraim888sun <ephraim888sun@gmail.com>
2022-03-22 16:16:33 -0500

    update backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import About from '/container/About';
+// import Footer from './container/Footer';
+// import Header from './container/Header';
+// import Skills from './container/Skills';
+// import Testimonial from './container/Testimonial';
+// import Work from './container/Work';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <About />
+        {/* <Footer />
+        <Header />
+        <Skills />
+        <Testimonial />
+        <Work /> */}
+        
     </div>
   );
 }
