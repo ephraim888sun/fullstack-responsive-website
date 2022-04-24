d11658a044cc0d923246125e1903aa20819f14e0
ephraim888sun <ephraim888sun@gmail.com>
2022-04-24 16:43:45 -0500

    included a toggle button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { About, Footer, Header, Skills, Testimonial, Work } from './container';
-import { Navbar, Toggle } from './components';
-import './App.scss';
+import React from 'react'
+import { About, Footer, Header, Skills, Testimonial, Work } from './container'
+import { Navbar } from './components'
+import './App.scss'
 
 function App() {
   return (
-    <div className="app">
-        {/* <Toggle /> */}
-        <Navbar />
-        <Header />
-        <About />
-        <Work />
-        <Skills />
-        <Testimonial />
-        <Footer />
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
